d661c6f773adc8dbb1e688886eae0d3688342f52
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-02-03 19:47:54 +0000

    ready to implement discard

--- spec/features/viewing_basket_spec.rb
+++ spec/features/viewing_basket_spec.rb
@@ -14,4 +14,17 @@ RSpec.feature 'Viewing basket' do
     end
   end
 
+
+  scenario 'I click the discard button' do
+    Product.create(name: 'Almond Toe Court Shoes', colour: 'Patent Black', gender: 'Women\'s', category: 'Footwear', previous_price: 10000, price: 9900, stockroom_count: 5)
+    visit '/'
+    click_on 'BUY'
+    visit '/'
+    click_link 'View your basket'
+    expect(page.status_code).to eq 200
+    expect(page).to have_content("1 Item(s) In Basket")
+    click_on 'discard'
+    expect(page).to have_content("0 Item(s) In Basket")
+  end
+
 end
